56aededda46535a743b260578d182a0b5f3c0b00
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2022-10-14 11:38:09 -0300

    Making login interface using components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,23 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
   const Component = asChild ? Slot : 'button';
 
   return (
     <Component
       className={clsx(
-        'w-full py-4 px-3 bg-cyan-500 rounded font-semibold text-sm text-black transition-colors hover:bg-cyan-300 focus:ring-2 focus:ring-white',
+        'w-full py-3 px-4 bg-cyan-500 rounded font-semibold text-sm text-black transition-colors hover:bg-cyan-300 focus:ring-2 focus:ring-white',
+        className,
       )}
+      {...props}
     >
       {children}
     </Component>
